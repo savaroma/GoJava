488a61d40aca2dbfa0c06da56ce110d9999b4363
Roman Savinuik <savaroma@gmail.com>
2015-12-20 02:20:41 +0200

    добавил муз. шоп

---
+++ src/lesson3/musicShop/Main.java
@@ -0,0 +1,7 @@
+package lesson3.musicShop;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lesson3/musicShop/MusicShop.java
@@ -0,0 +1,4 @@
+package lesson3.musicShop;
+
+public class MusicShop {
+}
