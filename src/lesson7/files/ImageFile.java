08f9b921f7cc0276bc3332bad6732f8e104e64fe
Roman Savinuik <savaroma@gmail.com>
2016-01-17 14:32:54 +0200

    lesson 3 - antipatterns

---
+++ src/lesson7/files/ImageFile.java
@@ -0,0 +1,143 @@
+package lesson7.files;
+
+public class ImageFile extends File {
+    private static String fileType = "img";
+
+    public ImageFile() {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(UNNAMED_FILE); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(Directory dir) {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(UNNAMED_FILE); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(Directory dir, String content) {
+        super.setContent(content);
+        super.setName(UNNAMED_FILE); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name) {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, String content) {
+        super.setContent(content);
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, Directory dir) {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, String content, Directory dir) {
+        super.setContent(content);
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, Directory dir, Integer size) {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, String content, Directory dir, Integer size) {
+        super.setContent(content);
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, Integer size) {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, String content, Integer size) {
+        super.setContent(content);
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(false);
+    }
+
+    public ImageFile(String name, Directory dir, Integer size, boolean readOnly) {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(readOnly);
+    }
+
+    public ImageFile(String name, String content, Directory dir, Integer size, boolean readOnly) {
+        super.setContent(content);
+        super.setName(name);
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(readOnly);
+    }
+
+    public ImageFile(Directory dir, Integer size, boolean readOnly) {
+        super.setContent(INIT_EMPTY_CONTENT); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setName(UNNAMED_FILE); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(readOnly);
+    }
+
+    public ImageFile(Directory dir, String content, Integer size, boolean readOnly) {
+        super.setContent(content);
+        super.setName(UNNAMED_FILE); // Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setExt(fileType);// Anti pattern hard coding Unnamed file name extracting to CONST in File class
+        super.setParentDir(dir);
+        super.setSize(INIT_SIZE);//Anti pattern hard coding 0 extracting to CONST in File class
+        super.setSize(size);
+        super.setReadOnly(readOnly);
+    }
+}
---
+++ src/lesson7/files/Main.java
@@ -0,0 +1,35 @@
+package lesson7.files;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Directory rootDir = new Directory();
+        rootDir.showDir();
+
+        TextFile dontReadme = new TextFile("dontReadme", rootDir);
+        dontReadme.fileInfo();
+
+        Directory musicDir = new Directory("Music", rootDir);
+        musicDir.showDir();
+
+        AudioFile sheakspeare = new AudioFile("sonata-5", musicDir, 1024000, true);
+        sheakspeare.fileInfo();
+
+        TextFile readme = new TextFile("readme", musicDir);
+        readme.fileInfo();
+
+        Directory picturesDir = new Directory("Pictures", rootDir);
+        picturesDir.showDir();
+
+        TextFile readmeFirst = new TextFile("readmeFirst", picturesDir);
+        readmeFirst.fileInfo();
+
+        Directory documents = new Directory("Documents", rootDir);
+        documents.showDir();
+
+        TextFile install = new TextFile("install", "Это контент файла", documents, 100);
+        install.fileInfo();
+
+
+    }
+}
